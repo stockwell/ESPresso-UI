40353c45469e068310fc4df6c0a97d18eb3c8fb3
stockwell <stockywell@gmail.com>
2022-05-29 21:47:52 +1000

    Brew Tab: Fix formatting

--- EspressoBrewTab.hpp
+++ EspressoBrewTab.hpp
@@ -4,34 +4,34 @@
 
 #include "BoilerController.hpp"
 
-class EspressoBrewTab : public BoilerTemperatureDelegate
+class EspressoBrewTab
+	: public BoilerTemperatureDelegate
 {
 public:
-    EspressoBrewTab(lv_obj_t* parent, BoilerController* boiler);
-    virtual ~EspressoBrewTab() = default;
+	EspressoBrewTab(lv_obj_t* parent, BoilerController* boiler);
+	virtual ~EspressoBrewTab() = default;
 
-    // BoilerTemperatureDelegate i/f
-    void onBoilerCurrentTempChanged(float temp) override;
-    void onBoilerTargetTempChanged(float temp) override;
+	// BoilerTemperatureDelegate i/f
+	void onBoilerCurrentTempChanged(float temp) override;
+	void onBoilerTargetTempChanged(float temp) override;
 
 private:
-    enum
-    {
-        indic_temp,
-        indic_arc,
-    };
+	enum
+	{
+		indic_temp,
+		indic_arc,
+	};
 
-    uint64_t                m_stopwatchTime = 0;
-    float                   m_targetTemp = 0.0f;
+	uint64_t m_stopwatchTime = 0;
+	float m_targetTemp = 0.0f;
 
-    lv_obj_t*               m_meter1;
-    lv_obj_t*               m_switch2;
-    lv_obj_t*               m_arcLabel;
+	lv_obj_t* m_meter1;
+	lv_obj_t* m_switch2;
+	lv_obj_t* m_arcLabel;
 
-    lv_meter_indicator_t*   m_indic[2];
+	lv_meter_indicator_t* m_indic[2];
 
-    lv_timer_t*             m_timer;
+	lv_timer_t* m_timer;
 
-    BoilerController::BoilerState   m_boilerState = BoilerController::BoilerState::Heating;
-    BoilerController*               m_boilerController;
+	BoilerController* m_boilerController;
 };
