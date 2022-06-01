28a76d1f341a372b2bbb8bfcb9068ae04fb09ad4
stockwell <stockywell@gmail.com>
2022-06-02 00:34:05 +1000

    UI: Updated Logo

--- EspressoUI.cpp
+++ EspressoUI.cpp
@@ -48,18 +48,18 @@ void EspressoUI::init(BoilerController* boiler)
 	lv_obj_t* logo = lv_img_create(tab_btns);
 	LV_IMG_DECLARE(espresso_logo);
 	lv_img_set_src(logo, &espresso_logo);
-	lv_obj_align(logo, LV_ALIGN_LEFT_MID, -LV_HOR_RES / 2 + 10, 0);
+	lv_obj_align(logo, LV_ALIGN_LEFT_MID, -LV_HOR_RES / 2 + 5, 2);
 
 	lv_obj_t* label1 = lv_label_create(tab_btns);
 	lv_label_set_text(label1, "ESPresso v0.01");
-	lv_obj_align_to(label1, logo, LV_ALIGN_OUT_RIGHT_TOP, 5, 0);
+	lv_obj_align_to(label1, logo, LV_ALIGN_OUT_RIGHT_TOP, -55, 10);
 	lv_obj_set_style_text_font(label1, font_large, 0);
 
 	lv_obj_t* label2 = lv_label_create(tab_btns);
 	lv_label_set_text(label2, "Gaggia Classic Pro");
 	lv_obj_set_style_text_font(label2, font_normal, 0);
 	lv_obj_set_style_text_opa(label2, LV_OPA_70, 0);
-	lv_obj_align_to(label2, logo, LV_ALIGN_OUT_RIGHT_TOP, 5, 22);
+	lv_obj_align_to(label2, logo, LV_ALIGN_OUT_RIGHT_TOP, -35, 32);
 
 	lv_obj_t* t1 = lv_tabview_add_tab(tv, "Brew");
 	lv_obj_t* t2 = lv_tabview_add_tab(tv, "Settings");
