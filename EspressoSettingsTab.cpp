a251e5bcd5295813c5de660e0e611b37365bfa5d
stockwell <stockywell@gmail.com>
2022-06-12 08:56:20 +1000

    Settings: Initial support for brew pressure

--- EspressoSettingsTab.cpp
+++ EspressoSettingsTab.cpp
@@ -9,20 +9,20 @@ static lv_obj_t* createLabel(lv_obj_t* parent, const char* text)
 	return label;
 }
 
-static void temperatureSliderCb(lv_event_t* e)
+static void sliderCb(lv_event_t* e)
 {
 	lv_obj_t* slider = lv_event_get_target(e);
-	auto [label, key] = *static_cast<std::pair<lv_obj_t*, std::string>*>(lv_event_get_user_data(e));
+	auto [label, key, fmt] = *static_cast<std::tuple<lv_obj_t*, std::string, std::string>*>(lv_event_get_user_data(e));
 	auto val = lv_slider_get_value(slider);
 
-	lv_label_set_text_fmt(label, "%d°c", val);
+	lv_label_set_text_fmt(label, fmt.c_str(), val);
 
 	auto& settings = SettingsManager::get();
 	settings[key] = static_cast<float>(val);
 	settings.save();
 }
 
-static std::pair<lv_obj_t*, lv_obj_t*> createTemperatureSlider(lv_obj_t* parent, const std::string& key, const std::pair<int, int>& range)
+static std::pair<lv_obj_t*, lv_obj_t*> createSlider(lv_obj_t* parent, const std::string& key, const std::pair<int, int>& range, const std::string& fmt)
 {
 	auto slider = lv_slider_create(parent);
 	auto initial = SettingsManager::get()[key].getAs<float>();
@@ -33,9 +33,9 @@ static std::pair<lv_obj_t*, lv_obj_t*> createTemperatureSlider(lv_obj_t* parent,
 
 	auto label = lv_label_create(parent);
 	lv_obj_set_style_text_font(label, &lv_font_montserrat_16, LV_PART_MAIN);
-	lv_label_set_text_fmt(label, "%d°c", static_cast<int>(initial));
+	lv_label_set_text_fmt(label, fmt.c_str(), static_cast<int>(initial));
 
-	lv_obj_add_event_cb(slider, temperatureSliderCb, LV_EVENT_VALUE_CHANGED, new std::pair<lv_obj_t*, std::string>(label, key));
+	lv_obj_add_event_cb(slider, sliderCb, LV_EVENT_VALUE_CHANGED, new std::tuple<lv_obj_t*, std::string, std::string>(label, key, fmt));
 
 	return { slider, label };
 }
@@ -43,18 +43,28 @@ static std::pair<lv_obj_t*, lv_obj_t*> createTemperatureSlider(lv_obj_t* parent,
 EspressoSettingsTab::EspressoSettingsTab(lv_obj_t* parent)
 : m_parent(parent)
 {
-	lv_obj_set_flex_flow(m_parent, LV_FLEX_FLOW_ROW_WRAP);
+	lv_obj_set_flex_flow(m_parent, LV_FLEX_FLOW_ROW);
+
+	static lv_coord_t grid1_col_dsc[] = {LV_GRID_CONTENT, LV_GRID_TEMPLATE_LAST};
+	static lv_coord_t grid1_row_dsc[] = {LV_GRID_CONTENT, LV_GRID_CONTENT, LV_GRID_TEMPLATE_LAST};
+	lv_obj_set_grid_dsc_array(parent, grid1_col_dsc, grid1_row_dsc);
 
 	lv_obj_t* cont = lv_obj_create(parent);
-	lv_obj_set_size(cont, 760, 240);
+	lv_obj_set_size(cont, 760, 110);
+
+	lv_obj_t* cont2 = lv_obj_create(parent);
+	lv_obj_set_size(cont2, 760, 65);
+
+	lv_obj_set_grid_cell(cont,  LV_GRID_ALIGN_START, 0, 1, LV_GRID_ALIGN_START, 0, 1);
+	lv_obj_set_grid_cell(cont2, LV_GRID_ALIGN_START, 0, 1, LV_GRID_ALIGN_START, 1, 1);
 
 	auto& settings = SettingsManager::get();
 
-	auto [slider1, sliderlabel] = createTemperatureSlider(cont, "BrewTemp", {85, 100});
-	auto [slider2, sliderlabel2] = createTemperatureSlider(cont, "SteamTemp", {120, 150});
+	auto [slider1, sliderlabel] = createSlider(cont, "BrewTemp", {85, 100}, "%d°c");
+	auto [slider2, sliderlabel2] = createSlider(cont, "SteamTemp", {120, 150}, "%d°c");
 
 	static lv_coord_t grid_col_dsc[] = {LV_GRID_CONTENT, LV_GRID_CONTENT, LV_GRID_TEMPLATE_LAST};
-	static lv_coord_t grid_row_dsc[] = {LV_GRID_CONTENT, LV_GRID_CONTENT, LV_GRID_CONTENT, LV_GRID_CONTENT, LV_GRID_TEMPLATE_LAST};
+	static lv_coord_t grid_row_dsc[] = {LV_GRID_CONTENT, LV_GRID_CONTENT, LV_GRID_CONTENT, LV_GRID_TEMPLATE_LAST};
 	lv_obj_set_grid_dsc_array(cont, grid_col_dsc, grid_row_dsc);
 
 	lv_obj_set_grid_cell(createLabel(cont, "Brew Temp."),  LV_GRID_ALIGN_START, 0, 1, LV_GRID_ALIGN_START, 0, 1);
@@ -64,4 +74,14 @@ EspressoSettingsTab::EspressoSettingsTab(lv_obj_t* parent)
 	lv_obj_set_grid_cell(createLabel(cont, "Steam Temp."),  LV_GRID_ALIGN_START, 0, 1, LV_GRID_ALIGN_START, 2, 1);
 	lv_obj_set_grid_cell(slider2, LV_GRID_ALIGN_START, 1, 1, LV_GRID_ALIGN_START, 2, 1);
 	lv_obj_set_grid_cell(sliderlabel2, LV_GRID_ALIGN_CENTER, 1, 1, LV_GRID_ALIGN_CENTER, 2, 1);
+
+
+	static lv_coord_t grid2_col_dsc[] = {LV_GRID_CONTENT, LV_GRID_CONTENT, LV_GRID_TEMPLATE_LAST};
+	static lv_coord_t grid2_row_dsc[] = {LV_GRID_CONTENT, LV_GRID_TEMPLATE_LAST};
+	lv_obj_set_grid_dsc_array(cont2, grid2_col_dsc, grid2_row_dsc);
+
+	auto [slider3, sliderlabel3] = createSlider(cont2, "BrewPressure", {6, 12}, "%d bar");
+	lv_obj_set_grid_cell(createLabel(cont2, "Brew Pressure"),  LV_GRID_ALIGN_START, 0, 1, LV_GRID_ALIGN_START, 0, 1);
+	lv_obj_set_grid_cell(slider3, LV_GRID_ALIGN_START, 1, 1, LV_GRID_ALIGN_START, 0, 1);
+	lv_obj_set_grid_cell(sliderlabel3, LV_GRID_ALIGN_CENTER, 1, 1, LV_GRID_ALIGN_CENTER, 0, 1);
 }
--- Settings/SettingsManagerDefaults.cpp
+++ Settings/SettingsManagerDefaults.cpp
@@ -4,6 +4,7 @@ void SettingsManager::loadDefaults()
 {
 	m_settings["BrewTemp"] = 93.0f;
 	m_settings["SteamTemp"] = 145.0f;
+	m_settings["BrewPressure"] = 9.0f;
 
 	save();
 }
