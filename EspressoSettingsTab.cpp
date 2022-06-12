50bec79c04354631fe1207aae478c3e8527aec84
stockwell <stockywell@gmail.com>
2022-06-12 16:17:23 +1000

    Settings: Initial support for setting boiler PID terms

--- EspressoSettingsTab.cpp
+++ EspressoSettingsTab.cpp
@@ -45,9 +45,9 @@ EspressoSettingsTab::EspressoSettingsTab(lv_obj_t* parent)
 {
 	lv_obj_set_flex_flow(m_parent, LV_FLEX_FLOW_ROW);
 
-	static lv_coord_t grid1_col_dsc[] = {LV_GRID_CONTENT, LV_GRID_TEMPLATE_LAST};
-	static lv_coord_t grid1_row_dsc[] = {LV_GRID_CONTENT, LV_GRID_CONTENT, LV_GRID_TEMPLATE_LAST};
-	lv_obj_set_grid_dsc_array(parent, grid1_col_dsc, grid1_row_dsc);
+	static lv_coord_t parent_grid_col_dsc[] = {LV_GRID_CONTENT, LV_GRID_TEMPLATE_LAST};
+	static lv_coord_t parent_grid_row_dsc[] = {LV_GRID_CONTENT, LV_GRID_CONTENT, LV_GRID_CONTENT, LV_GRID_TEMPLATE_LAST};
+	lv_obj_set_grid_dsc_array(parent, parent_grid_col_dsc, parent_grid_row_dsc);
 
 	lv_obj_t* cont = lv_obj_create(parent);
 	lv_obj_set_size(cont, 760, 110);
@@ -55,11 +55,16 @@ EspressoSettingsTab::EspressoSettingsTab(lv_obj_t* parent)
 	lv_obj_t* cont2 = lv_obj_create(parent);
 	lv_obj_set_size(cont2, 760, 65);
 
+	lv_obj_t* boilerPID_container = lv_obj_create(parent);
+	lv_obj_set_size(boilerPID_container, 760, 130);
+
 	lv_obj_set_grid_cell(cont,  LV_GRID_ALIGN_START, 0, 1, LV_GRID_ALIGN_START, 0, 1);
 	lv_obj_set_grid_cell(cont2, LV_GRID_ALIGN_START, 0, 1, LV_GRID_ALIGN_START, 1, 1);
+	lv_obj_set_grid_cell(boilerPID_container, LV_GRID_ALIGN_START, 0, 1, LV_GRID_ALIGN_START, 2, 1);
 
 	auto& settings = SettingsManager::get();
 
+	// Temperature Settings Container
 	auto [slider1, sliderlabel] = createSlider(cont, "BrewTemp", {85, 100}, "%d°c");
 	auto [slider2, sliderlabel2] = createSlider(cont, "SteamTemp", {120, 150}, "%d°c");
 
@@ -75,7 +80,7 @@ EspressoSettingsTab::EspressoSettingsTab(lv_obj_t* parent)
 	lv_obj_set_grid_cell(slider2, LV_GRID_ALIGN_START, 1, 1, LV_GRID_ALIGN_START, 2, 1);
 	lv_obj_set_grid_cell(sliderlabel2, LV_GRID_ALIGN_CENTER, 1, 1, LV_GRID_ALIGN_CENTER, 2, 1);
 
-
+	// Pressure Settings Container
 	static lv_coord_t grid2_col_dsc[] = {LV_GRID_CONTENT, LV_GRID_CONTENT, LV_GRID_TEMPLATE_LAST};
 	static lv_coord_t grid2_row_dsc[] = {LV_GRID_CONTENT, LV_GRID_TEMPLATE_LAST};
 	lv_obj_set_grid_dsc_array(cont2, grid2_col_dsc, grid2_row_dsc);
@@ -84,4 +89,25 @@ EspressoSettingsTab::EspressoSettingsTab(lv_obj_t* parent)
 	lv_obj_set_grid_cell(createLabel(cont2, "Brew Pressure"),  LV_GRID_ALIGN_START, 0, 1, LV_GRID_ALIGN_START, 0, 1);
 	lv_obj_set_grid_cell(slider3, LV_GRID_ALIGN_START, 1, 1, LV_GRID_ALIGN_START, 0, 1);
 	lv_obj_set_grid_cell(sliderlabel3, LV_GRID_ALIGN_CENTER, 1, 1, LV_GRID_ALIGN_CENTER, 0, 1);
+
+	// BoilerPID Container
+	static lv_coord_t boilerPID_grid_col_dsc[] = {LV_GRID_CONTENT, LV_GRID_CONTENT, LV_GRID_TEMPLATE_LAST};
+	static lv_coord_t boilerPID_grid_row_dsc[] = {LV_GRID_CONTENT, LV_GRID_CONTENT, LV_GRID_CONTENT, LV_GRID_CONTENT, LV_GRID_CONTENT, LV_GRID_TEMPLATE_LAST};
+
+	lv_obj_set_grid_dsc_array(boilerPID_container, boilerPID_grid_col_dsc, boilerPID_grid_row_dsc);
+
+	auto [slider4, sliderlabel4] = createSlider(boilerPID_container, "BoilerKp", {1, 500}, "%d");
+	lv_obj_set_grid_cell(createLabel(boilerPID_container, "Kp Term"),  LV_GRID_ALIGN_START, 0, 1, LV_GRID_ALIGN_START, 0, 1);
+	lv_obj_set_grid_cell(slider4, LV_GRID_ALIGN_START, 1, 1, LV_GRID_ALIGN_START, 0, 1);
+	lv_obj_set_grid_cell(sliderlabel4, LV_GRID_ALIGN_CENTER, 1, 1, LV_GRID_ALIGN_CENTER, 0, 1);
+
+	auto [slider5, sliderlabel5] = createSlider(boilerPID_container, "BoilerKi", {1, 500}, "%d");
+	lv_obj_set_grid_cell(createLabel(boilerPID_container, "Ki Term"),  LV_GRID_ALIGN_START, 0, 1, LV_GRID_ALIGN_START, 1, 1);
+	lv_obj_set_grid_cell(slider5, LV_GRID_ALIGN_START, 1, 1, LV_GRID_ALIGN_START, 1, 1);
+	lv_obj_set_grid_cell(sliderlabel5, LV_GRID_ALIGN_CENTER, 1, 1, LV_GRID_ALIGN_CENTER, 1, 1);
+
+	auto [slider6, sliderlabel6] = createSlider(boilerPID_container, "BoilerKd", {1, 500}, "%d");
+	lv_obj_set_grid_cell(createLabel(boilerPID_container, "Kd Term"),  LV_GRID_ALIGN_START, 0, 1, LV_GRID_ALIGN_START, 2, 1);
+	lv_obj_set_grid_cell(slider6, LV_GRID_ALIGN_START, 1, 1, LV_GRID_ALIGN_START, 2, 1);
+	lv_obj_set_grid_cell(sliderlabel6, LV_GRID_ALIGN_CENTER, 1, 1, LV_GRID_ALIGN_CENTER, 2, 1);
 }
